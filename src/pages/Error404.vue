5a521733de4a3b432ecd05dd510a2124b7945a59
Сандро <warsan00@mail.ru>
2020-09-19 14:14:23 +0500

    Правки

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="fixed-center text-center">
-    <p>
-      <img
-        src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
-      >
-    </p>
+    <p><img src="~assets/sad.svg" style="width: 30vw; max-width: 150px;"></p>
     <p class="text-faded"><strong>(404)</strong></p>
-    <q-btn
-      color="secondary"
-      style="width:200px;"
-      @click="$router.push('/')"
-    >Return</q-btn>
+    <q-btn color="secondary" style="width:200px;"
+      @click="$router.push('/')" >Return</q-btn>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Error404'
-}
+export default { name: 'Error404' }
 </script>
