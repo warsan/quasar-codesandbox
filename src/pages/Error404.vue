c6b454451fb44a241b7ea1df686d5694670207c2
Сандро <warsan00@mail.ru>
2020-09-19 13:39:34 +0500

    Update Error404.vue

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -6,12 +6,12 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-    <p class="text-faded">Извините, но здесь ничего нет...<strong>(404)</strong></p>
+    <p class="text-faded"><strong>(404)</strong></p>
     <q-btn
       color="secondary"
       style="width:200px;"
       @click="$router.push('/')"
-    >Вернуться</q-btn>
+    >Return</q-btn>
   </div>
 </template>
 
