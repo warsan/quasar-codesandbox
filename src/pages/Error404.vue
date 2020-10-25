d2994b807a9b3a0b825b70200fa2e90708a884dc
Сандро <warsan00@mail.ru>
2020-10-26 00:14:55 +0500

    Правки

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed-center text-center">
     <p><img src="~assets/sad.svg" style="width: 30vw; max-width: 150px;"></p>
-    <p class="text-faded">Извините, но здесь пусто...<strong>(404)</strong></p>
+    <p class="text-faded">Извините, но здесь пусто... <strong>(404)</strong></p>
     <q-btn color="secondary" style="width:200px;"
       @click="$router.push('/')" >Возврат</q-btn>
   </div>
--- src/store/module-example/index.js
+++ src/store/module-example/index.js
@@ -3,7 +3,6 @@ import * as actions from './actions'
 import * as getters from './getters'
 import * as mutations from './mutations'
 
-
 export default {
   namespaced: true,
   state,
