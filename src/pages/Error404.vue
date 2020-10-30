ea27b14b11d1d3d19e6a84459b1451d210149ef2
Сандро <warsan00@mail.ru>
2020-10-30 05:52:58 +0500

    Правки

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="fixed-center text-center">
-    <p><img src="~assets/sad.svg" style="width: 30vw; max-width: 150px;"></p>
+    <p><img src="~assets/sad.svg" style="width: 30vw; max-width: 150px;"/></p>
     <p class="text-faded">Извините, но здесь пусто... <strong>(404)</strong></p>
-    <q-btn color="secondary" style="width:200px;"
-      @click="$router.push('/')" >Возврат</q-btn>
+    <q-btn color="secondary" style="width:200px;" @click=$"router.push('/')">
+    Возврат
+    </q-btn>
   </div>
 </template>
 
